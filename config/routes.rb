9c40423b309fa9c437869ce673bec07ebfd7891c
ThibertChristophe <c.thibert89@gmail.com>
2023-12-30 23:06:41 +0100

    Creation du crud via resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # le GET articles va executer l action de index dans le ArticlesController
-  get '/articles', to: 'articles#index'
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', as: :rails_health_check
-
+  # get '/articles', to: 'articles#index'
+  # get '/articles/:id', to: 'articles#show'
+  # La ressource permet de creer les routes pour le CRUD sur ce controller
+  # Chose qu'on a fait manuellement au dessus avec le index(get) et show
+  resources :articles
   # Defines the root path route ("/")
   root 'articles#index'
 end
